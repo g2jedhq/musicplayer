fd9335be886dd8b50eae58c22fdb506331fa7644
Qubo <g2jedhq@qq.com>
2016-10-04 15:19:02 +0800

    使用缓存和文件存储图片

--- app/src/main/java/adapter/MusicAdapter.java
+++ app/src/main/java/adapter/MusicAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,10 +15,14 @@ import android.widget.TextView;
 
 import com.bobo.musicplayer.R;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.ref.SoftReference;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import entity.Music;
 import util.BitmapUtils;
@@ -41,6 +46,10 @@ public class MusicAdapter extends BaseAdapter {
      */
     private Thread workThread;
     private boolean isLoop = true;
+    /**
+     * 声明用于实现内存缓存的map，构造一个具有默认初始容量 (16) 和默认加载因子 (0.75) 的空 HashMap
+     */
+    private Map<String,SoftReference<Bitmap>> cache = new HashMap<>();
     /**
      * 声明handler  显示图片
      */
@@ -129,11 +138,33 @@ public class MusicAdapter extends BaseAdapter {
         // 组装数据和模板
         holder.tvName.setText(music.getTitle());
         holder.tvSinger.setText(music.getArtist_name());
-        //设置图片 向任务集合中添加一个图片下载任务
+        String path = music.getPic_small();
+        //设置图片  先从缓存中读取图片
+        SoftReference<Bitmap> ref = cache.get(path);
+        if (ref != null) {
+            Log.i("TAG", "从内存中获取的图片...");
+            Bitmap bitmap = ref.get();
+            if (bitmap!=null) {//图片还没有被销毁
+                holder.ivPic.setImageBitmap(bitmap);
+                return convertView;
+            }
+        }
+        // 内存缓存中没有图片  则去文件缓存中读取
+        String fileName = path.substring(path.lastIndexOf("/") + 1);
+        File file = new File(context.getCacheDir(),fileName);
+        Bitmap bitmap = BitmapUtils.loadBitmap(file.getAbsolutePath());
+        if (bitmap!=null) {
+            Log.i("TAG", "从文件中获取的图片....");
+            holder.ivPic.setImageBitmap(bitmap);
+            //向内存缓存中再存一次
+            cache.put(path, new SoftReference<Bitmap>(bitmap));
+            return convertView;
+        }
+        // 如果缓存和文件中都没有，设置图片 向任务集合中添加一个图片下载任务
         //给imageView设置一个tag值  用于handler中通过tag值找到imageView
-        holder.ivPic.setTag(music.getPic_small());
+        holder.ivPic.setTag(path);
         ImageLoadTask imageLoadTask = new ImageLoadTask();
-        imageLoadTask.path = music.getPic_small();
+        imageLoadTask.path = path;
         imageLoadTasks.add(imageLoadTask);
         //唤醒工作线程  赶紧起来干活
         synchronized (workThread) {
@@ -183,8 +214,19 @@ public class MusicAdapter extends BaseAdapter {
         try {
             InputStream is = HttpUtils.get(path);
             // BitmapFactory.decodeStream(is)
-            // Bitmap bitmap = BitmapUtils.loadBitmap(is, 50, 50);// 压缩图片
-            return BitmapUtils.loadBitmap(is,50,50);
+             Bitmap bitmap = BitmapUtils.loadBitmap(is, 50, 50);// 压缩图片
+            //下载成功  把bitmap存入内存缓存中
+            cache.put(path, new SoftReference<Bitmap>(bitmap));
+            //存入文件缓存目录中
+            // path = http://musicdata.baidu.com/data2/pic/5b13c6847088bda14e02a681f962061e/271831977/271831977.jpg
+            // public String substring(int beginIndex),参数beginIndex - 起始索引（包括）
+            // 返回一个新的字符串，它是此字符串的一个子字符串。该子字符串从指定索引处的字符开始，直到此字符串末尾。
+            String fileName = path.substring(path.lastIndexOf("/")+1);
+            // public int lastIndexOf(String str)返回指定子字符串str在此字符串中最右边出现处的索引
+            // file --> /data/data/cn.tedu.music/cache/xxxxx.jpg
+            File file = new File(context.getCacheDir(),fileName);
+            BitmapUtils.saveBitmap(bitmap, file.getAbsolutePath());
+            return bitmap;
         } catch (IOException e) {
             e.printStackTrace();
         }
